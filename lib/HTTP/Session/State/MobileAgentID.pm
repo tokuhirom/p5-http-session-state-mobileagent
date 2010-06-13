9d48692961cc770fae3b9d75ed5ea61edd4fc1f4
tokuhirom <tokuhirom@gmail.com>
2010-06-14 01:56:14 +0900

    initial import

---
+++ lib/HTTP/Session/State/MobileAgentID.pm
@@ -0,0 +1,39 @@
+package HTTP::Session::State::MobileAgentID;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTTP::Session::State::MobileAgentID -
+
+=head1 SYNOPSIS
+
+  use HTTP::Session::State::MobileAgentID;
+
+=head1 DESCRIPTION
+
+HTTP::Session::State::MobileAgentID is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
