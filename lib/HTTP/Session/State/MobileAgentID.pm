ec3ec8bde2f837eed825ad0381520d7a52525748
tokuhirom <tokuhirom@gmail.com>
2010-10-13 19:00:55 +0900

    Checking in changes prior to tagging of version 0.44.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index c1bf522..035493c 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::Session::State::MobileAgentID
    
    +0.44
    +
    +        - doc enhancements(no feacure changes)
    +
     0.43
    
             - fixed testing issue(no feature changes)

--- lib/HTTP/Session/State/MobileAgentID.pm
+++ lib/HTTP/Session/State/MobileAgentID.pm
@@ -2,7 +2,7 @@ package HTTP::Session::State::MobileAgentID;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.43';
+our $VERSION = '0.44';
 
 use HTTP::Session::State::Base;
 use HTTP::MobileAgent 0.28;
